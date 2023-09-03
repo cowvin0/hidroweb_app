7f5b63213b39a7fef7ca7cf8669977b59a2d5846
gabrielpereira12345 <gabrielpereira00PereiraGabriel@outlook.com>
2023-09-03 08:08:58 -0300

    escolhendo tipo de estação

---
+++ hidro_automate.py
@@ -0,0 +1,34 @@
+from playwright.async_api import async_playwright
+from time import sleep
+import asyncio
+
+async def main():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+
+        context = await browser.new_context(
+                color_scheme='light',
+                viewport={'width': 800, 'height':600},
+                )
+
+        page = await context.new_page()
+
+        await page.goto('https://www.snirh.gov.br/hidroweb/serieshistoricas')
+
+        await page.evaluate('document.querySelector(".mat-select-arrow-wrapper").click()')
+
+        await page.evaluate('var station_type = document.querySelectorAll(".mat-option-text")')
+
+        await page.evaluate('station_type[1].click()')
+
+        municipio_input = page.locator('#mat-input-3')
+
+        await municipio_input.fill('pian')
+
+        breakpoint()
+
+        await page.wait_for_timeout(5000)
+
+        print(await page.title())
+
+asyncio.run(main())
